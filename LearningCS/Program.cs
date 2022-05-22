932f01b62baf2459884077f1aba27a0d831ea58b
XByc_sk76@itstepru.onmicrosoft.com <XByc_sk76@itstepru.onmicrosoft.com>
2022-05-23 00:20:01 +1000

    Matrix operations in progress

--- LearningCS/Program.cs
+++ LearningCS/Program.cs
@@ -9,16 +9,35 @@ do
     Console.Write("Введите размерность матриц: ");
     n = Convert.ToInt32(Console.ReadLine());
 
-    if (n == 0) Console.WriteLine("Размерность матрицы не может быть 0. Осуществите повторный ввод");
+    if ((n == 0) || (n < 0)) Console.WriteLine("Размерность матрицы не может быть 0 либо отрицательной. Осуществите повторный ввод");
 
-} while (n == 0);
+} while ((n == 0) || (n < 0));
 
-Console.WriteLine($"\nСоздаем матрицу размерности {n}\n");
+Console.WriteLine($"\nСоздаем матрицу A размерности {n}\n");
 
 int[,] arr1 = new int[n, n];
 
 FillRand(arr1, n);
 Print(arr1, n);
+Console.WriteLine();
+
+Console.WriteLine($"\nСоздаем матрицу B размерности {n}\n");
+
+int[,] arr2 = new int[n, n];
+
+FillRand(arr2, n);
+Print(arr2, n);
+Console.WriteLine();
+
+Console.WriteLine("--------------------------------------\n");
+
+Console.WriteLine($"Результат сложения матриц А и В:\n");
+
+int[,] arr3 = new int[n, n];
+
+SumArray(arr1, arr2, arr3, n);
+Print(arr3, n);
+Console.WriteLine();
 
 void FillRand(int[,] arr, int n)
 {
@@ -41,5 +60,16 @@ void Print(int[,] arr, int n)
             Console.Write($"{arr[i, j]}\t");
         }
         Console.WriteLine();
+    }
+}
+
+void SumArray(int[,] arr1, int[,] arr2, int[,] newArr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newArr[i, j] = arr1[i, j] + arr2[i, j];
+        }
     }
 }
